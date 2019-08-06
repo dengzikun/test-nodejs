85883be1b091469b05cfe82609bf2deca70c28ec
dengzikun <dengdp@sina.com>
2019-08-06 15:11:23 +0800

    improve

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
+const crypto = require('crypto')
+
+const secret = 'mysecret'
+
+function sign(secret, data) {
+  return (
+    'sha1=' +
+    crypto
+      .createHmac('sha1', secret)
+      .update(data)
+      .digest('hex')
+  )
+}
 
 app.post('/webhook', (req, res) => {
   if (req.headers['x-github-event']) {
+    const signature = request.headers['x-hub-signature']
+    if (signature === sign(secret, chunk.toString())) {
+      console.log('ok')
+    }
     console.log(req.headers)
   }
   res.status(200).send('ok')
