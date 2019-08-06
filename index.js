19b9f2d0884ad7391f8f3e855c883aadc2296e4c
dengzikun <dengdp@sina.com>
2019-08-06 15:52:52 +0800

    improve

--- index.js
+++ index.js
@@ -15,30 +15,49 @@ function sign(secret, data) {
   )
 }
 
-app.use(bodyParser.json())
+app.use(
+  bodyParser.json({
+    verify: function(req, res, buf, encoding) {
+      req.rawBody = buf
+    }
+  })
+)
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/webhook', (req, res) => {
-  console.log(req.body)
   if (req.headers['x-github-event']) {
-    const reqData = []
-    let size = 0
-    req.on('data', (data) => {
-      console.log('>>>req on')
-      reqData.push(data)
-      size += data.length
-    })
-    req.on('end', () => {
-      req.reqData = Buffer.concat(reqData, size)
-      const signature = req.headers['x-hub-signature']
-      if (signature === sign(secret, req.reqData)) {
-        console.log(req.headers)
-        console.log('ok')
-        res.status(200).send('ok')
-      }
-    })
+    req.reqData = Buffer.concat(reqData, size)
+    const signature = req.headers['x-hub-signature']
+    if (signature === sign(secret, req.rawBody)) {
+      console.log(req.headers)
+      console.log(req.body)
+      console.log('ok')
+      res.status(200).send('ok')
+    }
   }
 })
+
+// app.post('/webhook', (req, res) => {
+//   console.log(req.body)
+//   if (req.headers['x-github-event']) {
+//     const reqData = []
+//     let size = 0
+//     req.on('data', (data) => {
+//       console.log('>>>req on')
+//       reqData.push(data)
+//       size += data.length
+//     })
+//     req.on('end', () => {
+//       req.reqData = Buffer.concat(reqData, size)
+//       const signature = req.headers['x-hub-signature']
+//       if (signature === sign(secret, req.reqData)) {
+//         console.log(req.headers)
+//         console.log('ok')
+//         res.status(200).send('ok')
+//       }
+//     })
+//   }
+// })
 
 app.listen(8080, () => {
   console.log('listening at 8080')
